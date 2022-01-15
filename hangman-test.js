a4a89738cb934c6855f9c9279b9ad12264a3ba36
Maria Gonzalez <mlimgonz@gmail.com>
2022-01-15 11:27:17 -0500

    hangman-test

---
+++ hangman-test.js
@@ -0,0 +1,52 @@
+var queryString = document.location.search;
+var repoName = queryString.split('=')[1];
+score=repoName.split("%20")[0]
+pokeId=repoName.split("%20")[1] 
+
+
+// Variables 
+
+//accepted alphabet letters
+var alphabet = ['a','A','b','B','c','C','d','D','e','E','f','F','g','G','h','H','i','I','j','J','k','K','l','L','m','M','n','N','o','O','p','P','q','Q','r','R','s','S','t','T','u','U','v','V','w','W','x','X','y','Y','z','Z'];
+
+//potential words to guess
+var pokemon = ['bulbasaur', 'ivysaur', 'venusaur', 'charmander', 'charmeleon', 'charizard', 'squirtle', 'wartortle', 'blastoise', 'caterpie', 'metapod', 'butterfree', 'weedle', 'kakuna', 'beedrill', 'pidgey', 'pidgeotto', 'pidgeot', 'rattata', 'raticate', 'spearow', 'fearow', 'ekans', 'arbok', 'pikachu', 'raichu', 'sandshrew', 'sandslash', 'nidoran', 'nidorina', 'nidoqueen', 'nidorino', 'nidoking', 'clefairy', 'clefable', 'vulpix', 'ninetales', 'jigglypuff', 'wigglytuff', 'zubat', 'golbat', 'oddish', 'gloom', 'vileplume', 'paras', 'parasect', 'venonat', 'venomoth', 'diglett', 'dugtrio', 'meowth', 'persian', 'psyduck', 'golduck', 'mankey', 'primeape', 'growlithe', 'arcanine', 'poliwag', 'poliwhirl', 'poliwrath', 'abra', 'kadabra', 'alakazam', 'machop', 'machoke', 'machamp', 'bellsprout', 'weepinbell', 'victreebell', 'tentacool', 'tentacruel', 'geodude', 'graveler', 'golem', 'ponyta', 'rapidash', 'slowpoke', 'slowbro', 'magnemite', 'magneton', 'farfetchd', 'doduo', 'dodrio', 'seel', 'dewgong', 'grimer', 'muk', 'shellder', 'cloyster', 'gastly', 'haunter', 'gengar', 'onix', 'drowsee', 'hypno', 'krabby', 'kingler', 'voltorb', 'electrode', 'exeggcute', 'exeggutor', 'cubone', 'marowak', 'hitmonlee', 'hitmonchan', 'lickitung', 'koffing', 'weezing', 'rhyhorn', 'rhydon', 'chansey', 'tangela', 'kangaskhan', 'horsea', 'seadra', 'goldeen', 'seaking', 'staryu', 'starmie', 'mrmime', 'scyther', 'jynx', 'electabuzz', 'magmar', 'pinsir', 'tauros', 'magikarp', 'gyarados', 'lapras', 'ditto', 'eevee', 'vaporeon', 'jolteon', 'flareon', 'porygon', 'omanyte', 'omastar', 'kabuto', 'kabutops', 'aerodactyl', 'snorlax', 'articuno', 'zapdos', 'moltres', 'dratini', 'dragonair', 'dragonite', 'mewtwo', 'mew'];
+
+//guesses left
+var guesses = 10;
+
+//number of wins
+var wins = 0;
+
+//number of losses
+var losses = 0;
+
+//Pick a random pokemon from a list as the word to guess
+var answer = pokemon[Math.floor(Math.random()*pokemon.length)];
+
+
+//create '_' placeholders in HTML document with the same number of characters as the pokemon
+var answerLength = answer.length;
+
+
+//function runs when user presses a key
+document.onkeyup = function(event) {
+    if (guesses === 0 || underscore.join('') === answer){
+    //    prevents guessing after game end
+    }
+    else {
+        //userGuess is recorded based on key pressed
+        var userGuess = event.key;
+
+        //limits guesses to alphabet letters
+        if (alphabet.indexOf(userGuess) > -1) {
+
+      
+    //announces a loss with the answer
+        document.getElementById("whosthatpokemon").innerHTML = "You lose! It's " + answer.toUpperCase()+"!";
+
+
+    //game reset
+        gameOver()
+    }
+}
