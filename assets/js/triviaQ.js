16867abe6a86e052cd188032978a6a705aacee70
Bowen Zheng <bxz5089@gmail.com>
2022-01-20 20:44:13 -0500

    Updated CSS and added icons

--- assets/js/triviaQ.js
+++ assets/js/triviaQ.js
@@ -12,7 +12,7 @@ var answer = ""
 var score = 0
 var questionNumber = 0
 var totalQuestions = 5
-var seconds=5
+var seconds=10
 
 function getDifficulty(){
     var queryString = document.location.search;
@@ -91,7 +91,7 @@ function checkIfCorrect() {
         scoreEl.textContent = score
     }
     questionNumber++
-    seconds=5
+    seconds=10
     nextQ()
 }
 function nextQ(){
@@ -110,7 +110,7 @@ function timer(){
         timerEl.textContent=seconds
         seconds--
         if(seconds<0){
-            seconds=5
+            seconds=10
             questionNumber++
             if(questionNumber<totalQuestions){
                 pick4names(apiData)
